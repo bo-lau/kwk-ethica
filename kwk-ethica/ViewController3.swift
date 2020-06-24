c8ed3c55569b158235a8534c359f76b715cdae60
bo-lau <boyee.lau12@gmail.com>
2020-06-24 14:59:59 -0400

    storyboard

---
+++ kwk-ethica/ViewController3.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController3.swift
+//  kwk-ethica
+//
+//  Created by Bo Lau on 2020-06-24.
+//  Copyright © 2020 Bo Lau. All rights reserved.
+//
+
+import UIKit
+
+class ViewController3: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
